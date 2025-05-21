99b166c6f960bd92a5cdaf78645407b2c6f06726
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-05-21 23:41:02 +1000

    commit code

---
+++ Terraform Expressions/ConditionalExpression/variables.tf
@@ -0,0 +1,8 @@
+# String type
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g., dev, prod, staging)"
+  default     = "dev"
+}
+
+
---
+++ Terraform Expressions/Splat Expression/variables.tf
@@ -0,0 +1,15 @@
+# String type
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g., dev, prod, staging)"
+  default     = "dev"
+}
+
+# List type
+variable "account_names" {
+  type        = list(string)
+  description = "List of allowed Azure storage accounts"
+  default     = ["2016mystorageacc1", "2016mystorageacc2", "2016mystorageacc3"]
+}
+
+
