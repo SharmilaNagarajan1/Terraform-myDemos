2591b7a22ae4915da100c509c33316dd689433a0
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-05-22 17:36:04 +1000

    Add files via upload

---
+++ Storing State-File To Remote/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 4.8.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-day04"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "day045433"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+  required_version = ">=1.9.0"
+}
+
+provider "azurerm" {
+    features {
+      
+    }
+  
+}
+
+variable "environment" {
+       type = string
+       description = "the env type"
+       default = "staging"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "remotestate-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+ 
+  name                     = "storagedemo2017mithy"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location # implicit dependency
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = var.environment
+  }
+}
