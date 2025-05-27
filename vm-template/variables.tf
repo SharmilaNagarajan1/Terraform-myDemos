d0862311b59041da07e3d54205be1caa4bfc53d5
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-05-27 12:41:26 +1000

    Add files via upload

---
+++ vm-template/variables.tf
@@ -0,0 +1,13 @@
+
+variable "resource_group_location" {
+  type        = string
+  default     = "westus"
+  description = "Location of the resource group."
+}
+
+
+variable "resource_group_name" {
+  type        = string
+  default     = "kml_rg_main-2ac0b2cfde6d4d3a"
+  description = "Resource group name in your Azure subscription."
+}
