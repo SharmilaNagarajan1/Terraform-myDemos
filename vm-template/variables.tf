b3a0b3bce8981c22a37240262388348776e05048
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-06-16 21:50:38 +1000

    Update variables.tf

--- vm-template/variables.tf
+++ vm-template/variables.tf
@@ -8,6 +8,6 @@ variable "resource_group_location" {
 
 variable "resource_group_name" {
   type        = string
-  default     = "kml_rg_main-2ac0b2cfde6d4d3a"
+  default     = " "
   description = "Resource group name in your Azure subscription."
 }
